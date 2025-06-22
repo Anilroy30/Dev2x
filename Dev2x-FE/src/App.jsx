3ca13eafb7e39b05cb1f93841a0709f295f47a72
Striver <anilkumarakula34@gmail.com>
2025-06-22 10:56:03 +0530

    shwowing feed

--- Dev2x-FE/src/App.jsx
+++ Dev2x-FE/src/App.jsx
@@ -1,18 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./components/Body";
 import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Connections from "./components/Connections";
 
 
 function App() {
   return (
     <>
+    <Provider store={appStore}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Body/>}>
+            <Route path="/" element={<Feed/>}/>
             <Route path="/login" element={<Login/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+            <Route path="/connections" element={<Connections/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
+    </Provider>
     </>
   )
 }
