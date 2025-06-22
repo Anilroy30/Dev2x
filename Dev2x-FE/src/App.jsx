b43388f1e89a19222725b1f32098f9ea2d543e0a
Striver <anilkumarakula34@gmail.com>
2025-06-22 10:57:07 +0530

    Integrated requests route

--- Dev2x-FE/src/App.jsx
+++ Dev2x-FE/src/App.jsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./components/Body";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
+import Feed from "./components/Feed";
 import Connections from "./components/Connections";
+import Requests from "./components/Requests";
 
 
 function App() {
@@ -16,6 +18,7 @@ function App() {
             <Route path="/login" element={<Login/>}/>
             <Route path="/profile" element={<Profile/>}/>
             <Route path="/connections" element={<Connections/>}/>
+            <Route path="/requests" element={<Requests/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
